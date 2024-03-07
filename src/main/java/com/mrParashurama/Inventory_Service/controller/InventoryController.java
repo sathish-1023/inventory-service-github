a45cbc5f6384a9f4107c3b470c949a5f22a13f70
Sathish Projects <kalivimarsa705@gmail.com>
2024-03-07 20:00:16 +0530

    deploying using dockerfile some modifications in all

--- src/main/java/com/mrParashurama/Inventory_Service/controller/InventoryController.java
+++ src/main/java/com/mrParashurama/Inventory_Service/controller/InventoryController.java
@@ -1,22 +1,22 @@
 package com.mrParashurama.Inventory_Service.controller;
 
-import com.mrParashurama.Inventory_Service.dto.InventoryResponse;
-import com.mrParashurama.Inventory_Service.service.InventoryService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/inventory")
-@RequiredArgsConstructor
-public class InventoryController {
-
-    private final InventoryService inventoryService;
-    @GetMapping()
-    @ResponseStatus(HttpStatus.OK)
-    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
-            return inventoryService.isInStock(skuCode);
-    }
-}
+//import com.mrParashurama.Inventory_Service.dto.InventoryResponse;
+//import com.mrParashurama.Inventory_Service.service.InventoryService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/inventory")
+//@RequiredArgsConstructor
+//public class InventoryController {
+//
+//    private final InventoryService inventoryService;
+//    @GetMapping()
+//    @ResponseStatus(HttpStatus.OK)
+//    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+//            return inventoryService.isInStock(skuCode);
+//    }
+//}
